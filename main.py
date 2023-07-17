64811c6fe68f5a7eef0b40dd811c845b03f350eb
unclechuen <johnny@johnnyip.com>
2023-07-18 01:29:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,11 +33,16 @@ async def handle_message(client, message):
         chat_ids = []
 
     if message.chat.id in chat_ids:
-        # print(message)
+        print(message)
         # Format the date and time
-        # media_datetime = message.date.strftime('%Y-%m-%d_%H-%M-%S')
+        # media_datetime = message.date.strftime('%Y-%m-%d_%H-%M-%S'
+        
         # Create the file path
+        # Non-Mac
         file_path_prefix = f"{message.chat.title}_{message.message_id}_{message.from_user.username}"
+        # Mac
+        # file_path_prefix = f"{message.chat.title}_{message.id}_{message.from_user.username}"
+
         # Check the type of the message and download if it's a type we're interested in
         if message.audio: 
             file_path = await client.download_media(message=message, file_name=f"{file_path_prefix}.mp3")
