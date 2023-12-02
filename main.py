9f99b9be41b42b10c949657fdeb4e10013b9e323
unclechuen <johnny@johnnyip.com>
2023-12-02 20:45:40 +0800

    Delay seconds before sending

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from os.path import splitext
 import asyncio
 import os
+import time
 import mimetypes
 from dotenv import load_dotenv
 
@@ -24,6 +25,7 @@ async def handle_message(client, message):
     # Check if the message is from a chat in your list
     target_chat = os.getenv("CHAT_TARGET")
     chat_ids = os.getenv("CHAT_IDS")    
+    delay_seconds = os.getenv("DELAY_SECONDS")
 
     # Get chat id array
     if chat_ids is not None:
@@ -39,12 +41,7 @@ async def handle_message(client, message):
         target_chat = []
 
     if message.chat.id in chat_ids:
-        # print(message)
-        # Format the date and time
-        # media_datetime = message.date.strftime('%Y-%m-%d_%H-%M-%S'
-        
         # Create the file path
-        # file_path_prefix = f"{message.chat.title}_{message.date}_{message.from_user.username}"
         file_path_prefix = f"/app/downloads/{message.chat.id}/{message.id}_{message.from_user.username}"
         file_path_prefix = file_path_prefix.replace(" ", "_")
         # print(message)
@@ -53,35 +50,50 @@ async def handle_message(client, message):
         # Check the type of the message and download if it's a type we're interested in
         if message.audio: 
             file_path = await client.download_media(message=message, file_name=f"{file_path_prefix}.mp3")
-            print(f"Downloaded audio file to {file_path}")
+            print(f"[{datetime.now()}]Downloaded audio file to {file_path}")
             # Send the downloaded media to another user or group
             for chat in target_chat:
+                time.sleep(delay_seconds)
                 await client.send_audio(chat_id=chat, audio=file_path)
+                print(f"[{datetime.now()}]File sent to chat: {chat}")
+
         elif message.photo:
             file_path = await client.download_media(message=message, file_name=f"{file_path_prefix}.jpg")
-            print(f"Downloaded photo file to {file_path}")
+            print(f"[{datetime.now()}]Downloaded photo file to {file_path}")
             for chat in target_chat:
+                time.sleep(delay_seconds)
                 await client.send_photo(chat_id=chat, photo=file_path)
+                print(f"[{datetime.now()}]File sent to chat: {chat}")
+
         elif message.video:
             file_path = await client.download_media(message=message, file_name=f"{file_path_prefix}.mp4")
-            print(f"Downloaded video file to {file_path}")
+            print(f"[{datetime.now()}]Downloaded video file to {file_path}")
             for chat in target_chat:
+                time.sleep(delay_seconds)
                 await client.send_video(chat_id=chat, video=file_path)
+                print(f"[{datetime.now()}]File sent to chat: {chat}")
+
         elif message.document:
             # Get file extension based on the MIME type, if the filename is not set
             file_path = await client.download_media(message=message, file_name=f"{file_path_prefix}_{message.document.file_name}")
-            print(f"Downloaded document file to {file_path}")
+            print(f"[{datetime.now()}]Downloaded document file to {file_path}")
             for chat in target_chat:
+                time.sleep(delay_seconds)
                 await client.send_document(chat_id=chat, document=file_path)
+                print(f"[{datetime.now()}]File sent to chat: {chat}")
+
         elif message.animation:
             file_path = await client.download_media(message=message, file_name=f"{file_path_prefix}.gif")
-            print(f"Downloaded animation file to {file_path}")
+            print(f"[{datetime.now()}]Downloaded animation file to {file_path}")
             for chat in target_chat:
+                time.sleep(delay_seconds)
                 await client.send_animation(chat_id=chat, animation=file_path)
+                print(f"[{datetime.now()}]File sent to chat: {chat}")
+
 
 
     else:
-        print(f"Message from chat {message.chat.id} ignored")
+        print(f"[{datetime.now()}]Message from chat {message.chat.id} ignored: {message.text}")
 
 
 app.run()
