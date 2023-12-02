0dc9ed6b9df07559996b2231b460ce526852a207
unclechuen <johnny@johnnyip.com>
2023-12-02 21:41:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,59 +25,74 @@ async def handle_message(client, message):
     # Check if the message is from a chat in your list
     target_chat = [int(id) if id.isdigit() else id for id in os.getenv("CHAT_TARGET").split(',')] if os.getenv("CHAT_TARGET") is not None else []
     chat_ids = [int(id) for id in os.getenv("CHAT_IDS").split(',')] if os.getenv("CHAT_IDS") is not None else []
-    ignore_username = os.getenv("CHAT_IGNORE_USERNAME").split(",") if os.getenv("CHAT_IGNORE_USERNAME") else []
+    ignore_username = [username.strip() for username in os.getenv("CHAT_IGNORE_USERNAME").split(",")] if os.getenv("CHAT_IGNORE_USERNAME") else []
     delay_seconds = int(os.getenv("DELAY_SECONDS", 5))
+    # print(target_chat)
+    # print(chat_ids)
+    # print(ignore_username)
     # print(message)
+    # print(message.from_user)
 
-    if message.chat.id in chat_ids and (message.from_user is not None and message.from_user.username not in ignore_username):
-        # Create the file path
-        file_path_prefix = f"/app/downloads/{message.chat.id}/"
-        file_path_prefix = file_path_prefix.replace(" ", "_")
+    # print(f"1: {message.chat.id in chat_ids}")
+    # print(f"2: {message.from_user is not None}")
+    # print(f"3: {message.from_user.username not in ignore_username}")
+
+    if message.chat.id in chat_ids:
+
+        if (message.from_user is not None and message.from_user.username in ignore_username):
+            print(f"[{datetime.now()}]Message from ignored user {message.from_user.username} chat {message.chat.id}: {message.text}")
+        
+        else:
+            # Create the file path
+            file_path_prefix = f"/app/downloads/{message.chat.id}/"
+            file_path_prefix = file_path_prefix.replace(" ", "_")
 
 
-        # Check the type of the message and download if it's a type we're interested in
-        if message.audio: 
-            file_path = await client.download_media(message=message, file_name=f"{file_path_prefix}/{message.id}_{message.from_user.username}.mp3")
-            print(f"[{datetime.now()}]Downloaded audio file to {file_path}")
-            # Send the downloaded media to another user or group
-            for chat in target_chat:
-                time.sleep(delay_seconds)
-                await client.send_audio(chat_id=chat, audio=file_path)
-                print(f"[{datetime.now()}]File sent to chat: {chat}")
+            # Check the type of the message and download if it's a type we're interested in
+            if message.audio: 
+                file_path = await client.download_media(message=message, file_name=f"{file_path_prefix}/{message.id}_{message.from_user.username}.mp3")
+                print(f"[{datetime.now()}]Downloaded audio file to {file_path}")
+                # Send the downloaded media to another user or group
+                for chat in target_chat:
+                    time.sleep(delay_seconds)
+                    await client.send_audio(chat_id=chat, audio=file_path)
+                    print(f"[{datetime.now()}]File sent to chat: {chat}")
 
-        elif message.photo:
-            file_path = await client.download_media(message=message, file_name=f"{file_path_prefix}/{message.id}_{message.from_user.username}.jpg")
-            print(f"[{datetime.now()}]Downloaded photo file to {file_path}")
-            for chat in target_chat:
-                time.sleep(delay_seconds)
-                await client.send_photo(chat_id=chat, photo=file_path)
-                print(f"[{datetime.now()}]File sent to chat: {chat}")
+            elif message.photo:
+                file_path = await client.download_media(message=message, file_name=f"{file_path_prefix}/{message.id}_{message.from_user.username}.jpg")
+                print(f"[{datetime.now()}]Downloaded photo file to {file_path}")
+                for chat in target_chat:
+                    time.sleep(delay_seconds)
+                    await client.send_photo(chat_id=chat, photo=file_path)
+                    print(f"[{datetime.now()}]File sent to chat: {chat}")
 
-        elif message.video:
-            file_path = await client.download_media(message=message, file_name=f"{file_path_prefix}/{message.id}_{message.from_user.username}.mp4")
-            print(f"[{datetime.now()}]Downloaded video file to {file_path}")
-            for chat in target_chat:
-                time.sleep(delay_seconds)
-                await client.send_video(chat_id=chat, video=file_path)
-                print(f"[{datetime.now()}]File sent to chat: {chat}")
+            elif message.video:
+                file_path = await client.download_media(message=message, file_name=f"{file_path_prefix}/{message.id}_{message.from_user.username}.mp4")
+                print(f"[{datetime.now()}]Downloaded video file to {file_path}")
+                for chat in target_chat:
+                    time.sleep(delay_seconds)
+                    await client.send_video(chat_id=chat, video=file_path)
+                    print(f"[{datetime.now()}]File sent to chat: {chat}")
 
-        elif message.document:
-            # Get file extension based on the MIME type, if the filename is not set
-            file_path = await client.download_media(message=message, file_name=f"{file_path_prefix}/{message.id}_{message.from_user.username}_{message.document.file_name}")
-            print(f"[{datetime.now()}]Downloaded document file to {file_path}")
-            for chat in target_chat:
-                time.sleep(delay_seconds)
-                await client.send_document(chat_id=chat, document=file_path)
-                print(f"[{datetime.now()}]File sent to chat: {chat}")
+            elif message.document:
+                # Get file extension based on the MIME type, if the filename is not set
+                file_path = await client.download_media(message=message, file_name=f"{file_path_prefix}/{message.id}_{message.from_user.username}_{message.document.file_name}")
+                print(f"[{datetime.now()}]Downloaded document file to {file_path}")
+                for chat in target_chat:
+                    time.sleep(delay_seconds)
+                    await client.send_document(chat_id=chat, document=file_path)
+                    print(f"[{datetime.now()}]File sent to chat: {chat}")
 
-        elif message.animation:
-            file_path = await client.download_media(message=message, file_name=f"{file_path_prefix}/{message.id}_{message.from_user.username}.gif")
-            print(f"[{datetime.now()}]Downloaded animation file to {file_path}")
-            for chat in target_chat:
-                time.sleep(delay_seconds)
-                await client.send_animation(chat_id=chat, animation=file_path)
-                print(f"[{datetime.now()}]File sent to chat: {chat}")
+            elif message.animation:
+                file_path = await client.download_media(message=message, file_name=f"{file_path_prefix}/{message.id}_{message.from_user.username}.gif")
+                print(f"[{datetime.now()}]Downloaded animation file to {file_path}")
+                for chat in target_chat:
+                    time.sleep(delay_seconds)
+                    await client.send_animation(chat_id=chat, animation=file_path)
+                    print(f"[{datetime.now()}]File sent to chat: {chat}")
 
+            else:
+                print(f"[{datetime.now()}]Text message from user {message.from_user.username} chat {message.chat.id} ignored: {message.text}")
 
 
     else:
